aa4f91a7f0100848e7129037c705942952391e29
John Perez <tatozan@gmail.com>
2022-10-12 22:47:00 -0300

    segundo desafio

---
+++ desafio-2/src/components/ItemListContainer.jsx
@@ -0,0 +1,11 @@
+import './ItemListContainer.css';
+
+import React from 'react'
+
+const ItemListContainer = ({greeting}) => {
+  return (
+    <div>{greeting}</div>
+  );
+}
+
+export default ItemListContainer;
